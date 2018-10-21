6ec3469c13c079c0987bbbd14e5de552935b5c64
Kaitlin <kaitlin.wan@gmail.com>
2018-10-21 18:44:17 -0400

    update landing

--- app.py
+++ app.py
@@ -4,10 +4,16 @@
 #2018-10-15
 
 from flask import Flask,request,render_template,session,url_for,redirect,flash
+import os
 
+app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = open("secret.txt","r").read()
+#app.secret_key = open("secret.txt","r").read()
 
 @app.route("/")
 def register():
-    
+    return render_template("landing.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
