0196bed87fcd0503484d3b25a81d2887dcabf927
KaitlinWan <kwan1@stuy.edu>
2018-10-24 11:44:54 -0400

    update app.py and deleted working

--- app.py
+++ app.py
@@ -5,7 +5,8 @@
 
 from flask import Flask,request,render_template,session,url_for,redirect,flash
 import os
-from util import auth,entries
+from util import auth
+import sqlite3
 
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
@@ -13,36 +14,28 @@ app.secret_key = "supersecretkey"
 
 @app.route("/")
 def home():
-
+    #If there is a session: take user to dashbaord
     if 'user' in session: #ALREADY LOGGED IN NEVER LOGGED OUT
         print("user in ses")
         return redirect("dash.html")
-
-    if not("user" in request.args): #NOT LOGGED IN NEW USER FIRST TIME
-        return render_template("landing.html")
-
-    login = auth.login(request.args['user'],request.args['pass'])
-    print(login)
-    if login == 0: #Page with error msg
-        return render_template("template.html", loginStatus = "unsucessful")
-    else:
-        session['user'] = request.args["user"]
-        return redirect('/landing.html')
     return render_template("landing.html")
-
 
 
 @app.route("/login")
 def login():
     que = request.args
-    print(que)
+    login = auth.login(request.args['user'],request.args['pass'])
+    if (login == 0):
+        return render_template("landing.html", loginStatus = "Invalid Login.")
     return render_template("dash.html")
 
 @app.route("/register")
 def reg():
     que = request.args
-    auth.register(que['user'],que['pass'])
-    return render_template("landing.html")
+    if (auth.register(que['user'],que['pass']) == 0):
+        return render_template("landing.html", loginStatus = "Nope. Try again. Already registered")
+    else:
+        return render_template("landing.html", loginStatus = "Sucessfully Registered.")
 
 if __name__ == '__main__':
     app.debug = True
